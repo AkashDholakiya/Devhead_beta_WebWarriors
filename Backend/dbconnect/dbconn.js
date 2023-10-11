f03c70d62624880a3a6684d4f7ca7a006f72acde
AkashDholakiya <116069339+AkashDholakiya@users.noreply.github.com>
2023-10-11 21:33:29 +0530

    Update dbconn.js

--- Backend/dbconnect/dbconn.js
+++ Backend/dbconnect/dbconn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://akashdholakiya:Ak%40sh635692@cluster0.0tjtlea.mongodb.net/FitPal?retryWrites=true&w=majority' 
+const uri = process.env.MONGO_URI
 function dbconn(){
     mongoose.connect(process.env.MONGO_URI || uri);
     console.log('Connected to MongoDB');      
